70cdb21c0ed53a897a7f34b761377f3bd74ce3cd
renaanc <renaanc95@gmail.com>
2025-05-05 19:54:12 -0300

    database att

--- biospace_site/settings.py
+++ biospace_site/settings.py
@@ -95,11 +95,14 @@ WSGI_APPLICATION = 'biospace_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'biospace',         # nome do banco que você criou
+        'USER': 'postgres',      # usuário do PostgreSQL
+        'PASSWORD': 'cldia',    # senha do usuário
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
