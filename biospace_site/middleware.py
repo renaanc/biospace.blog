80c6c15a8642a3ed6dc8ba8f5d89159baf86e303
renaanc <renaanc95@gmail.com>
2025-05-06 18:41:26 -0300

    middelware att

--- biospace_site/middleware.py
+++ biospace_site/middleware.py
@@ -4,6 +4,7 @@ from django.utils import translation
 
 class SetLanguageMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if request.path == "/" and not request.path.startswith(('/en/', '/pt-br/')):
+        if request.path == "/" and not request.COOKIES.get('django_language'):
             lang = translation.get_language_from_request(request)
             return HttpResponseRedirect(f"/{lang}/")
+
