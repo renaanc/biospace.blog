b323635f965a0f8a6be7a693cfe83f830d47ddb9
renaanc <renaanc95@gmail.com>
2025-05-07 13:20:45 -0300

    hosts att

--- biospace_site/settings.py
+++ biospace_site/settings.py
@@ -87,13 +87,16 @@ WSGI_APPLICATION = 'biospace_site.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True  # 👈 Importante para Railway
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
