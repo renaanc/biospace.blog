bced8a2804094fcb8eb68d60599894fe7404f827
renaanc <renaanc95@gmail.com>
2025-05-07 21:38:27 -0300

    general att's

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from parler.admin import TranslatableAdmin
 from .models import Artigo
+from django.utils.html import format_html
 
 
 
@@ -8,6 +9,12 @@ from .models import Artigo
 @admin.register(Artigo)
 class ArtigoAdmin(TranslatableAdmin):
     list_display = ['__str__', 'publicado_em']
+
+    def thumbnail_preview(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="{}" style="height: 60px;" />', obj.thumbnail.url)
+        return "-"
+    thumbnail_preview.short_description = "Thumbnail"
 
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'core'  # Esta linha é crucial para o namespace
 
@@ -7,4 +10,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
