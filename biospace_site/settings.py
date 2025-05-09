81fc904c694515c6caa7aee4ab8df6cc5bb02c99
renaanc <renaanc95@gmail.com>
2025-05-09 19:57:38 -0300

    Corrige CSRF  para Render7

--- biospace_site/settings.py
+++ biospace_site/settings.py
@@ -189,6 +189,6 @@ LOGGING = {
     },
 }
 
-CSRF_COOKIE_DOMAIN = ".onrender.com"
+CSRF_COOKIE_DOMAIN = "7kve.onrender.com"
 CSRF_TRUSTED_ORIGINS = ['https://biospace-7kve.onrender.com']
 SESSION_COOKIE_SECURE = True  # Para garantir que o cookie de sessão seja enviado apenas via HTTPS
