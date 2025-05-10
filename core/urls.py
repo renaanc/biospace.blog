67d6aa037fbefb2115cee44caf1bcc42e0b11a90
renaanc <renaanc95@gmail.com>
2025-05-10 00:12:53 -0300

    urls att

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import language_test_view
 
 app_name = 'core'  # Esta linha é crucial para o namespace
 
@@ -10,5 +9,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path("language-test/", language_test_view, name="language_test"),  # Adicionando a URL para o teste
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
