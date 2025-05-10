5268f9583028e4b3acc4937f0a3b76d0300f03ff
renaanc <renaanc95@gmail.com>
2025-05-10 00:21:44 -0300

    views  att

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, get_object_or_404  # Adicionei a importação do get_object_or_404
 from .models import Article
-from django.views.decorators.csrf import csrf_protect
-from django.shortcuts import render
 
-
-@csrf_protect
-def minha_view_com_post(request):
-    # Lógica de manipulação de dados POST aqui
-    return render(request, 'articles/article_detail.html')
 
 def article_list(request):
     # Filtrando apenas os artigos publicados
