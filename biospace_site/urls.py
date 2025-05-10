7a48aa15a74a10566ed2214d0534d6447cfee1c2
renaanc <renaanc95@gmail.com>
2025-05-09 22:26:34 -0300

    CSRF test1

--- biospace_site/urls.py
+++ biospace_site/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path
+from core.views import language_test_view
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),  # Rota de troca de idioma
 ]
@@ -15,5 +18,11 @@ urlpatterns += i18n_patterns(
     path('articles/', include('articles.urls', namespace='articles')),  # Isso vai incluir as URLs da app articles
 )
 
+urlpatterns = [
+    path("csrf-test/", language_test_view, name="csrf_test"),
+]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
